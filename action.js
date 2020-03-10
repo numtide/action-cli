5850960c3c7043cc217f6068feb75610fc50972d
zimbatm <zimbatm@zimbatm.com>
2020-03-10 11:31:55 +0100

    actions-cli action (#2)
    Add a GitHub Actions installer for the CLI

---
+++ action.js
@@ -0,0 +1,50 @@
+const child_process = require('child_process');
+const fs = require('fs');
+const http = require('http');
+const os = require('os');
+const path = require('path');
+
+// Get the version from the Cargo.toml file
+const cargoToml = fs.readFileSync(path.join(__dirname, 'Cargo.toml'));
+const version = /version *= *"([^"]+)"/.exec(cargoToml)[1];
+
+// Build the download URL
+const baseURL = "https://github.com/zimbatm/actions-cli/releases/download/v" + version;
+const system = os.platform() + "-" + os.arch()
+
+let filename;
+switch(system) {
+  case "linux-x64":
+    filename = "actions-cli-x86_64-unknown-linux-musl.tar.gz";
+    break;
+  case "darwin-x64":
+    filename = "actions-cli-x86_64-apple-darwin.tar.gz";
+    break;
+  case "win32-x64":
+    filename = "actions-cli-x86_64-pc-windows-msvc.zip";
+    break;
+  default:
+    throw "System " + system + " not supported";
+}
+const url = baseURL + "/" + filename;
+
+// Use the first PATH entry as the destination
+const binDir = process.env.PATH.split(path.delimiter)[0];
+
+// And finally download and unpack
+console.log("Installing", url, "to", binDir);
+
+switch(system) {
+  case "linux-x64":
+  case "darwin-x64":
+    child_process.execSync(`curl -fL ${url} | tar -xzC ${binDir}`)
+    break;
+  // TODO
+  //case "win32-x64":
+  default:
+    throw "System " + system + " not supported";
+}
+
+child_process.execSync('actions-cli --help >&2')
+console.log();
+console.log(`action-cli v${version} installed`);
